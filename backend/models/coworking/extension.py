72ae85ab48aca67f01ad7991a6d857fe907c0506
chloeecarroll <111141414+chloeecarroll@users.noreply.github.com>
2023-11-05 17:21:40 -0500

    Add ExtensionRequest model

--- backend/models/coworking/extension.py
+++ backend/models/coworking/extension.py
@@ -5,4 +5,5 @@ from backend.models.coworking.reservation import Reservation
 
 class ExtensionRequest(Reservation, BaseModel):
     current: Reservation | None = None
+    newStart: datetime | None = None
     newEnd: datetime | None = None
