45fbd147847116df1d4bc8055b65b1cc12bf383c
Lauren Jones <77810213+laurennnjones@users.noreply.github.com>
2023-11-12 17:50:53 -0500

    Add Backend Reservation Extension Logic

--- backend/api/coworking/reservation.py
+++ backend/api/coworking/reservation.py
@@ -3,6 +3,7 @@
 This API is used to make and manage reservations."""
 
 from fastapi import APIRouter, Depends, HTTPException
+from datetime import timedelta
 from ..authentication import registered_user
 from ...services.coworking.reservation import ReservationService
 from ...models import User
@@ -64,3 +65,24 @@ def cancel_reservation(
     return reservation_svc.change_reservation(
         subject, ReservationPartial(id=id, state=ReservationState.CANCELLED)
     )
+
+@api.post("/reservation/{id}/extend", tags=["Coworking"])
+def extend_reservation(
+    id: int,
+    extension_duration: timedelta,
+    subject: User = Depends(registered_user),
+    reservation_svc: ReservationService = Depends(),
+) -> Reservation:
+    """Extend a reservation's end time.
+    
+    This endpoint allows a user to extend their current reservation by up to an additional hour,
+    provided there is less than 30 minutes remaining in their current reservation.
+
+    Args:
+        id (int): The ID of the reservation to extend.
+        extension_duration (timedelta): The duration by which to extend the reservation, up to a maximum of one hour.
+        subject (User): The user requesting the reservation extension.
+
+    Returns:
+        Reservation: The updated reservation with the new end time."""
+    return reservation_svc.extend_reservation(subject, id, extension_duration)
