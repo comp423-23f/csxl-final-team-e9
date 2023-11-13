4b827acf72dc99255c1728919033deabb968db02
chloeecarroll <111141414+chloeecarroll@users.noreply.github.com>
2023-11-13 14:18:45 -0500

    Fill in extension fields (#49)

--- backend/models/coworking/extension.py
+++ backend/models/coworking/extension.py
@@ -1,8 +1,16 @@
 from datetime import datetime
+from enum import Enum
 from pydantic import BaseModel
 from backend.models.coworking.reservation import Reservation
 
 
+class ExtensionState(str, Enum):
+    DRAFT = "DRAFT"
+    CONFIRMED = "CONFIRMED"
+    CANCELLED = "CANCELLED"
+
+
 class ExtensionRequest(Reservation, BaseModel):
+    state: ExtensionState
     current: Reservation | None = None
     newEnd: datetime | None = None
